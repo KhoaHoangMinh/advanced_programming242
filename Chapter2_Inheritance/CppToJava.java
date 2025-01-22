2438e5483800ff68346b8519857677c53a3045d7
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-01-22 20:55:19 +0700

    First commit

---
+++ Chapter2_Inheritance/CppToJava.java
@@ -0,0 +1,35 @@
+public class CppToJava {
+    public static class A {
+        protected int a;
+        public A() {
+            a = 10;
+        }
+        public int f() {
+            a++;
+            return a;
+        }
+        public int getA() {
+            return a;
+        }
+    }
+    public static class B extends A {
+        @Override
+        public int f() {
+            a--;
+            return a;
+        }
+    }
+    public static void main(String[] args) {
+        int n;
+        A a = new A();
+        A a2 = new A();
+        B b = new B();
+
+        a.f();
+        b.f();
+        a2 = b;
+        a2.f();
+
+        System.out.println(a.getA() + " " + b.getA() + " " + a2.getA());
+    }
+}
