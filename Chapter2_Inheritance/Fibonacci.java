267773ad04d8d68caf3dcbff7850746c8e287caf
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-02-20 07:15:08 +0700

    Added testcase

--- Chapter2_Inheritance/Fibonacci.java
+++ Chapter2_Inheritance/Fibonacci.java
@@ -12,8 +12,23 @@ public class Fibonacci {
         double fibo = 2.078087 * Math.log(n) + 1.672276;
         return (int) Math.round(fibo);
     }
+    public static void tc01(int n) {
+        int[] fibo = new int[n];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i = 2; i < n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        for(int i = 0; i < n; i++) {
+            if(isFibo(i)) {
+                System.out.println( findFiboIndex(i) + ": " + i);
+            } else {
+//                System.out.println(i + " is not a Fibonacci number");
+            }
+        }
+    }
     public static void main(String[] args) {
-        int n = 21;
-        System.out.println(findFiboIndex(n));
+        int n = 100;
+        tc01(n);
     }
 }
