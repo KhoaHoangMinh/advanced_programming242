1e7d70de68ff35b9206a6676fc58e3a638d5117f
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-03-05 22:20:27 +0700

    Add tc02

--- Chapter2_Inheritance/Fibonacci.java
+++ Chapter2_Inheritance/Fibonacci.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Fibonacci {
     // Determine if a number is Fibonacci and find it's order
     public static boolean isPerfectSquare(int n) {
@@ -12,6 +14,28 @@ public class Fibonacci {
         double fibo = 2.078087 * Math.log(n) + 1.672276;
         return (int) Math.round(fibo);
     }
+    public static int[] generateFibonacci(int n) {
+        if(n == 0) {
+            int[] arr = new int[1];
+            arr[0] = 0;
+            return arr;
+        } else if(n == 1) {
+            int[] arr = new int[2];
+            arr[0] = 0;
+            arr[1] = 1;
+            return arr;
+        } else {
+            int[] arr = new int[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+
+            for(int i = 2; i <= n; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr;
+        }
+
+    }
     public static void tc01(int n) {
         int[] fibo = new int[n];
         fibo[0] = 0;
@@ -19,7 +43,7 @@ public class Fibonacci {
         for(int i = 2; i < n; i++) {
             fibo[i] = fibo[i - 1] + fibo[i - 2];
         }
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i <= n; i++) {
             if(isFibo(i)) {
                 System.out.println( findFiboIndex(i) + ": " + i);
             } else {
@@ -27,8 +51,23 @@ public class Fibonacci {
             }
         }
     }
+    public static void tc02(int n) {
+        int[] arr = new int[n + 1];
+        int[] fiboArr = generateFibonacci(n);
+        int[] idx = new int[n + 1];
+        for(int i = 0; i < n + 1; i++) {
+            arr[i] = i;
+        }
+        System.out.println(Arrays.toString(fiboArr));
+        for(int i = 0; i < n + 1; i++) {
+            idx[i] = findFiboIndex(fiboArr[i]);
+
+        }
+        System.out.println(Arrays.toString(idx));
+        System.out.println(Arrays.toString(arr));
+    }
     public static void main(String[] args) {
-        int n = 100;
-        tc01(n);
+        int n = 10;
+        tc02(n);
     }
 }
