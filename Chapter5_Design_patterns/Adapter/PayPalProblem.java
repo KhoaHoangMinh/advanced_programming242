9ad3088905dcc6b188f74f9eec5af206abbed19b
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-03-20 08:40:19 +0700

    First commit

---
+++ Chapter5_Design_patterns/Adapter/PayPalProblem.java
@@ -0,0 +1,39 @@
+class PayPal {
+    public void construct() {}
+    public void sendPayment(int amount) {
+        System.out.println("Paying via PayPal " + amount);
+    }
+}
+class MoneyBooker {
+    public void payMoney(int amount) {
+        System.out.println("Paying via MoneyBooker " + amount);
+    }
+}
+interface Adapter {
+    public void pay(int amount);
+}
+class PayPalAdapter implements Adapter {
+    PayPal payPal;
+    PayPalAdapter(PayPal obj) {payPal = obj;}
+    @Override
+    public void pay(int amount) {payPal.sendPayment(amount);}
+}
+class MoneyBookerAdapter implements Adapter {
+    MoneyBooker moneyBooker;
+    MoneyBookerAdapter(MoneyBooker obj) {moneyBooker = obj;}
+    @Override
+    public void pay(int amount) {
+        moneyBooker.payMoney(amount);
+    }
+}
+public class PayPalProblem {
+    public static void main(String[] args) {
+        PayPal payPal = new PayPal();
+        PayPalAdapter payPalAdapter = new PayPalAdapter(payPal);
+        payPalAdapter.pay(100000);
+
+        MoneyBooker moneyBooker = new MoneyBooker();
+        MoneyBookerAdapter moneyBookerAdapter = new MoneyBookerAdapter(moneyBooker);
+        moneyBookerAdapter.pay(5000);
+    }
+}
