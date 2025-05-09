06c1c4079f69546516c9a498d460231bad5171c6
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-05-09 15:09:43 +0700

    First commit

--- Chapter10_Functions/main.py
+++ Chapter10_Functions/main.py
@@ -18,17 +18,16 @@ if __name__ == '__main__':
     f4 = (lambda x: lambda y: x + 4 * y)(5)(2)
     f5 = (lambda x: lambda y: x)(5)(2)
     f6 = (lambda x: (lambda y: x + 4 * y))((lambda z: z * z )(4))(2)
-
-    f7_1 = lambda x: x(2)
-    f7_2 = lambda z: z + 1
-    f7 = f7_1(f7_2)
-    print("f7 = ", f7)
-
+    f7 = (lambda x: (lambda y: x + 4 * y)((lambda z: z**2)(4)))(2)
     f8 = (lambda x: lambda y: 2 * y + 3 * x)(7)
+    f9 = (lambda x: x(2))((lambda y: lambda z: 2 * y + 3 * z)(7))
 
-    f9_1 = lambda x: x(2)
-    f9_2 = (lambda y : lambda z: 2 * y + 3 * z)(7)
-    f9 = f9_1(f9_2)
+    f10 = (lambda x: lambda y: x - y)(5)(2) # 3
+    print("f10 = ", f10)
+    # How to assign x = 2, y = 5 only using brackets
+    f11 = (lambda x: lambda y: x - y)(5)(2) # -3
+    print("f11 = ", f10)
+
 
     # Understand reduce function
     #  Applies a particular function passed in its argument
